492dbdbd90a107c10cb7274a19c56b2d6513aadb
Annalisa Caldarulo <annalisacaldarulo97@gmail.com>
2025-03-25 19:25:11 +0100

    score

--- src/components/ResultModal.jsx
+++ src/components/ResultModal.jsx
@@ -1,23 +1,28 @@
 import React, { useImperativeHandle, useRef } from 'react'
 
-function ResultModal({ result, targetTime, ref }) {
+function ResultModal({ targetTime, ref, timeRemaining, onReset }) {
 
+    const formattedRemainingTime = (timeRemaining / 1000).toFixed(2)
+    // lo scopo è arrivare più vicino possibile a 0
+    const score = Math.round((1 - timeRemaining / (targetTime * 1000)) * 100)
+    // const score = (formattedRemainingTime/targetTime) * 100
+    const userLost = timeRemaining <= 0;
     const dialog = useRef()
 
     useImperativeHandle(ref, () => {
         return {
-            open() { 
+            open() {
                 dialog.current.showModal();
             }
         }
     });
     return (
-        <dialog ref={dialog} className='result-modal' >
-            <h2> You {result} </h2>
-            <p>The target time was  <span className='font-bold'>{targetTime} seconds.</span></p>
-            <p>You stopped teh timer with  <span className='font-bold'>X seconds left.</span></p>
-            <form method="dialog">
-                <button>close</button>
+        <dialog ref={dialog} className='result-modal' onClose={onReset} >
+            {userLost ? <h2> You lost</h2> : <h2>Your Score: {score}</h2>}
+            <p>The target time was  <span className='font-bold'>{targetTime} second{targetTime > 1 ? 's' : ''}.</span></p>
+            <p>You stopped the timer with  <span className='font-bold'>{formattedRemainingTime} second{formattedRemainingTime > 1 ? 's' : ''} left.</span></p>
+            <form method="dialog" onSubmit={onReset}>
+                <button >close</button>
             </form>
         </dialog>
     )
--- src/components/TimerChallenge.jsx
+++ src/components/TimerChallenge.jsx
@@ -3,34 +3,60 @@ import ResultModal from './ResultModal';
 
 function TimerChallenge({ title, targetTime }) {
 
-    const [timerStarted, setTimerStarted] = useState(false);
-    const [timerExpired, setTimerExpired] = useState(false);
+    // const [timerStarted, setTimerStarted] = useState(false);
+    // const [timerExpired, setTimerExpired] = useState(false);
 
+    const [remainingTime, setRemainingTime] = useState(targetTime * 1000);
     const timer = useRef(); // la variabile in cui salvo il timer, fuori così sarà accessibile da tutte le funzioni
     // siccome lo stato fa ripartire il componente la variabile timer viene inizializzata ogni volta QUINDI MEGLIO REF
 
     const dialog = useRef();
 
+    const timerIsActive = remainingTime > 0 && remainingTime < targetTime * 1000
+
+    if (remainingTime <= 0) {
+        clearInterval(timer.current);
+        dialog.current.open();
+
+    }
+    function handleReset() {
+        setRemainingTime(targetTime * 1000)
+
+    }
+
     //! funzione per startare il timer
     function handleStart() {
-        setTimerStarted(true);
+        // setTimerStarted(true);
 
-        timer.current = setTimeout(() => {
-            setTimerExpired(true);
-            dialog.current.open(); //! ora fa riferimento al ref che abbiamo creato noi
-        }, targetTime * 1000);
+        // timer.current = setTimeout(() => { 
+        //     setTimerExpired(true);
+        //     dialog.current.open(); //! ora fa riferimento al ref che abbiamo creato noi
+        // }, targetTime * 1000);
         //! Dopo i tot ms fa partire la callback (funzione di stato)
 
+        // dobbiamo calcolare quanti secondi mancano: quindi non va bene setTimeout ma setInterval
+        timer.current = setInterval(() => {
+            // dobbiamo tenere conto del tempo che rimane
+            setRemainingTime(prevTimeRemaining => prevTimeRemaining - 20);
+        }, 20) // non funziona come timeout, parte ogni volta che è passato il tempo segnato
     }
 
     function handleStop() {
         //! da questa funzione voglio stoppare il timer della funzione handleStart
-        clearTimeout(timer.current); // ha bisogno del pointer del timer
+        // clearTimeout(timer.current); // ha bisogno del pointer del timer
+        dialog.current.open();
+        clearInterval(timer.current);
+
     }
 
     return (
         <>
-            <ResultModal ref={dialog} targetTime={targetTime} result="lost" />
+            <ResultModal
+                ref={dialog}
+                targetTime={targetTime}
+                timeRemaining={remainingTime}
+                onReset={handleReset}
+            />
             <section className='challenge'>
                 <h2>{title}</h2>
                 {/* {timerExpired && <p>You lost</p>} */}
@@ -38,12 +64,12 @@ function TimerChallenge({ title, targetTime }) {
                     {targetTime} second{targetTime > 1 ? 's' : ''}
                 </p>
                 <p>
-                    <button onClick={timerStarted ? handleStop : handleStart}>
-                        {timerStarted ? 'Stop' : 'Start'} Challenge
+                    <button onClick={timerIsActive ? handleStop : handleStart}>
+                        {timerIsActive ? 'Stop' : 'Start'} Challenge
                     </button>
                 </p>
-                <p className={timerStarted ? 'active' : ''}>
-                    {timerStarted ? ' Time is running...' : 'Timer inactive'}
+                <p className={timerIsActive ? 'active' : ''}>
+                    {timerIsActive ? ' Time is running...' : 'Timer inactive'}
                 </p>
             </section>
         </>
