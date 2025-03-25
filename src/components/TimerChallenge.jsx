b4162904c1f772dc0978ada36891b0024a7ed6b1
Annalisa Caldarulo <annalisacaldarulo97@gmail.com>
2025-03-25 11:55:11 +0100

    start e stop del timer con state

--- src/components/TimerChallenge.jsx
+++ src/components/TimerChallenge.jsx
@@ -1,19 +1,42 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 function TimerChallenge({ title, targetTime }) {
+
+    const [timerStarted, setTimerStarted] = useState(false);
+    const [timerExpired, setTimerExpired] = useState(false);
+
+    let timer; // la variabile in cui salvo il timer, fuori così sarà accessibile da tutte le funzioni
+    //! funzione per startare il timer
+    function handleStart() {
+        setTimerStarted(true);
+
+        timer = setTimeout(() => {
+            setTimerExpired(true)
+        }, targetTime * 1000);
+        //! Dopo i tot ms fa partire la callback (funzione di stato)
+
+    }
+
+    function handleStop() {
+        //! da questa funzione voglio stoppare il timer della funzione handleStart
+        clearTimeout(timer); // ha bisogno del pointer del timer
+      
+    }
+
     return (
         <section className='challenge'>
             <h2>{title}</h2>
+            {timerExpired && <p>You lost</p>}
             <p className='challenge-time'>
                 {targetTime} second{targetTime > 1 ? 's' : ''}
             </p>
             <p>
-                <button>
-                    Start/Stop Challenge
+                <button onClick={timerStarted ? handleStop : handleStart}>
+                    {timerStarted ? 'Stop' : 'Start'} Challenge
                 </button>
             </p>
-            <p className=''>
-                Time is running.../Timer inactive
+            <p className={timerStarted ? 'active' : ''}>
+                {timerStarted ? ' Time is running...' : 'Timer inactive'}
             </p>
         </section>
     )
